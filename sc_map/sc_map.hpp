6aa75f7a3cc597d86f3ea5795110f878bfc60612
Chris <amstutz@kit.edu>
2014-03-12 10:47:24 +0100

    add sc_map.hpp for easier include sc_map.hpp combines all the files related to sc_map, including containers and iterators.

---
+++ sc_map/sc_map.hpp
@@ -0,0 +1,24 @@
+/*!
+ * @file sc_map.hpp
+ * @author Christian Amstutz
+ * @date Mar 12, 2014
+ *
+ * @brief
+ *
+ */
+
+/*
+ *  Copyright (c) 2014 by Christian Amstutz
+ */
+
+#pragma once
+
+#include "sc_map_base.hpp"
+#include "sc_map_linear.hpp"
+#include "sc_map_square.hpp"
+#include "sc_map_cube.hpp"
+#include "sc_map_4d.hpp"
+
+#include "sc_map_iterator.hpp"
+#include "sc_map_iter_sequential.hpp"
+#include "sc_map_iter_square.hpp"
