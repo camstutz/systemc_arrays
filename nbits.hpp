88dbe23055e790b28bc33ffe8bd061abd3f36911
Chris <amstutz@kit.edu>
2014-03-24 11:39:21 +0100

    change some values to generic

--- nbits.hpp
+++ nbits.hpp
@@ -1,17 +1,15 @@
 /*!
- * @file preproc_log2.hpp
+ * @file nbits.hpp
  * @author Christian Amstutz
- * @date Mar 20, 2014
+ * @date Mar 24, 2014
  *
  * @brief
  *
  */
 
-#define NBITS2(n) ((n&2)?1:0)
-#define NBITS4(n) ((n&(0xC))?(2+NBITS2(n>>2)):(NBITS2(n)))
-#define NBITS8(n) ((n&0xF0)?(4+NBITS4(n>>4)):(NBITS4(n)))
-#define NBITS16(n) ((n&0xFF00)?(8+NBITS8(n>>8)):(NBITS8(n)))
-#define NBITS32(n) ((n&0xFFFF0000)?(16+NBITS16(n>>16)):(NBITS16(n)))
-#define NBITS64(n) ((n&0xFFFFFFFF00000000)?(32+NBITS32(n>>32)):(NBITS32(n)))
-#define NBITS128(n) ((n&0xFFFFFFFFFFFFFFFF0000000000000000)?(64+NBITS64(n>>64)):(NBITS64(n)))
-#define NBITS(n) (n==0?0:NBITS128(n)+1)
+#define NBITS2(n) (((n)&2)?1:0)
+#define NBITS4(n) (((n)&(0xC))?(2+NBITS2((n)>>2)):(NBITS2(n)))
+#define NBITS8(n) (((n)&0xF0)?(4+NBITS4((n)>>4)):(NBITS4(n)))
+#define NBITS16(n) (((n)&0xFF00)?(8+NBITS8((n)>>8)):(NBITS8(n)))
+#define NBITS32(n) (((n)&0xFFFF0000)?(16+NBITS16((n)>>16)):(NBITS16(n)))
+#define NBITS(n) ((n)==0?0:NBITS32(n)+1)
