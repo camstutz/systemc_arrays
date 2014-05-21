3ccc0890cffc3743d4e80e1af3a6a35330ce3b86
Chris <amstutz@kit.edu>
2014-05-21 09:37:12 +0200

    update sc_analyzer with C++98 support The sc_analyzer contains now two versions one for C++11 with higher preceission and one for C++98 for compatibility. The version is selected by the compilation.

--- sc_analyzer/sc_analyzer.cpp
+++ sc_analyzer/sc_analyzer.cpp
@@ -1,7 +1,7 @@
 /*!
  * @file sc_analyzer.hpp
  * @author Christian Amstutz
- * @date Feb 19, 2014
+ * @date May 21, 2014
  *
  * @brief
  *
@@ -14,59 +14,81 @@
 #include "sc_analyzer.hpp"
 
 //******************************************************************************
-//void sc_analyzer::register_model_setup_start()
-//{
-//    setup_start_time = std::chrono::high_resolution_clock::now();
-//
-//    return;
-//}
-//
-////******************************************************************************
-//void sc_analyzer::register_model_setup_end()
-//{
-//    setup_end_time = std::chrono::high_resolution_clock::now();
-//    setup_end_registered = true;
-//
-//    return;
-//}
-//
-////******************************************************************************
-//void sc_analyzer::register_simulation_start()
-//{
-//    simulation_start_time = std::chrono::high_resolution_clock::now();
-//    if (setup_end_registered == false)
-//    {
-//        setup_end_time = simulation_start_time;
-//    }
-//
-//    return;
-//}
-//
-////******************************************************************************
-//void sc_analyzer::register_simulation_end()
-//{
-//    simulation_end_time = std::chrono::high_resolution_clock::now();
-//
-//    return;
-//}
-//
-////******************************************************************************
-//void sc_analyzer::print_report()
-//{
-//    std::chrono::duration<double> setup_time;
-//    std::chrono::duration<double> simulation_time;
-//
-//    setup_time = setup_end_time - setup_start_time;
-//    simulation_time = simulation_end_time - simulation_start_time;
-//
-//    std::cout << std::dec << std::endl;
-//    std::cout << "************************************\n";
-//    std::cout << "SystemC Analysis Report\n";
-//    std::cout << "************************************" << std::endl;
-//    std::cout << "Time to create model : " << setup_time.count()   << "s" << std::endl;
-//    std::cout << "Time to simulate     : " << simulation_time.count() << "s" << std::endl;
-//    std::cout << "Simulated Time       : " << sc_time_stamp() << std::endl;
-//    std::cout << "Delta Cycles         : " << sc_delta_count() << std::endl;
-//
-//    return;
-//}
+sc_analyzer::sc_analyzer() : setup_end_registered(false)
+{}
+
+//******************************************************************************
+void sc_analyzer::register_model_setup_start()
+{
+    setup_start_time = get_time();
+
+    return;
+}
+
+//******************************************************************************
+void sc_analyzer::register_model_setup_end()
+{
+    setup_end_time = get_time();
+    setup_end_registered = true;
+
+    return;
+}
+
+//******************************************************************************
+void sc_analyzer::register_simulation_start()
+{
+    simulation_start_time = get_time();
+    if (setup_end_registered == false)
+    {
+        setup_end_time = simulation_start_time;
+    }
+
+    return;
+}
+
+//******************************************************************************
+void sc_analyzer::register_simulation_end()
+{
+    simulation_end_time = get_time();
+
+    return;
+}
+
+//******************************************************************************
+void sc_analyzer::print_report()
+{
+    double setup_time_display;
+    double simulation_time_display;
+
+    duration_type setup_time = setup_end_time - setup_start_time;
+    duration_type simulation_time = simulation_end_time - simulation_start_time;
+
+#if __cplusplus < 201000
+    setup_time_display = (double)setup_time/CLOCKS_PER_SEC;
+    simulation_time_display = (double)simulation_time/CLOCKS_PER_SEC;
+#else
+    setup_time_display = setup_time.count();
+    simulation_time_display = simulation_time.count();
+#endif
+
+    std::cout << std::dec << std::endl;
+    std::cout << "************************************\n";
+    std::cout << "SystemC Analysis Report\n";
+    std::cout << "************************************" << std::endl;
+    std::cout << "Time to create model : " << setup_time_display   << "s" << std::endl;
+    std::cout << "Time to simulate     : " << simulation_time_display << "s" << std::endl;
+    std::cout << "Simulated Time       : " << sc_time_stamp() << std::endl;
+    std::cout << "Delta Cycles         : " << sc_delta_count() << std::endl;
+
+    return;
+}
+
+//******************************************************************************
+sc_analyzer::clock_type sc_analyzer::get_time()
+{
+#if __cplusplus < 201000
+    return (clock() );
+#else
+    return (std::chrono::high_resolution_clock::now() );
+#endif
+}
--- sc_analyzer/sc_analyzer.hpp
+++ sc_analyzer/sc_analyzer.hpp
@@ -1,7 +1,7 @@
 /*!
  * @file sc_analyzer.hpp
  * @author Christian Amstutz
- * @date Feb 19, 2014
+ * @date May 21, 2014
  *
  * @brief
  *
@@ -13,7 +13,12 @@
 
 #pragma once
 
-//#include <chrono>
+#if __cplusplus < 201000
+#include "time.h"
+#else
+#include <chrono>
+#endif
+
 #include <iostream>
 
 #include <systemc.h>
@@ -23,18 +28,31 @@
 class sc_analyzer
 {
 public:
-//    void register_model_setup_start();
-//    void register_model_setup_end();
-//    void register_simulation_start();
-//    void register_simulation_end();
-//
-//    void print_report();
+
+#if __cplusplus < 201000
+    typedef clock_t clock_type;
+    typedef clock_t duration_type;
+#else
+    typedef std::chrono::time_point<std::chrono::system_clock> clock_type;
+    typedef std::chrono::duration<double> duration_type;
+#endif
+
+    sc_analyzer();
+
+    void register_model_setup_start();
+    void register_model_setup_end();
+    void register_simulation_start();
+    void register_simulation_end();
+
+    void print_report();
 
 private:
-//    std::chrono::time_point<std::chrono::system_clock> setup_start_time;
-//    std::chrono::time_point<std::chrono::system_clock> setup_end_time;
-//    std::chrono::time_point<std::chrono::system_clock> simulation_start_time;
-//    std::chrono::time_point<std::chrono::system_clock> simulation_end_time;
+    clock_type setup_start_time;
+    clock_type setup_end_time;
+    clock_type simulation_start_time;
+    clock_type simulation_end_time;
 
-    //bool setup_end_registered = false;
+    bool setup_end_registered;
+
+    clock_type get_time();
 };
