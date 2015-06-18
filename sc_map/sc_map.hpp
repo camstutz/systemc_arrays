e159285bb96fd874648000a590b2ab86f7b129e7
Christian Amstutz <amstutz@kit.edu>
2015-06-18 15:17:24 +0200

    add additional headers to the main header of sc_map

--- sc_map/sc_map.hpp
+++ sc_map/sc_map.hpp
@@ -1,7 +1,7 @@
 /*!
  * @file sc_map.hpp
  * @author Christian Amstutz
- * @date June 17, 2015
+ * @date June 18, 2015
  *
  * @brief
  *
@@ -14,8 +14,23 @@
 #pragma once
 
 #include "sc_map_base.hpp"
+
 #include "sc_map_list/sc_map_list.hpp"
+#include "sc_map_list/sc_map_list_key.hpp"
+#include "sc_map_list/sc_map_list_range.hpp"
+
 #include "sc_map_linear/sc_map_linear.hpp"
+#include "sc_map_linear/sc_map_linear_key.hpp"
+#include "sc_map_linear/sc_map_linear_range.hpp"
+
 #include "sc_map_square/sc_map_square.hpp"
+#include "sc_map_square/sc_map_square_key.hpp"
+#include "sc_map_square/sc_map_square_range.hpp"
+
 #include "sc_map_cube/sc_map_cube.hpp"
+#include "sc_map_cube/sc_map_cube_key.hpp"
+#include "sc_map_cube/sc_map_cube_range.hpp"
+
 #include "sc_map_4d/sc_map_4d.hpp"
+#include "sc_map_4d/sc_map_4d_key.hpp"
+#include "sc_map_4d/sc_map_4d_range.hpp"
