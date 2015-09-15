0d9bf0338b120654c0592d6b64b7cb37aeecc028
Christian Amstutz <amstutz@kit.edu>
2015-09-15 14:07:01 +0200

    add name binding for sc_map_list

--- sc_map/include/sc_map_list.hpp
+++ sc_map/include/sc_map_list.hpp
@@ -33,6 +33,9 @@ public:
     typedef typename base::key_type key_type;
     typedef typename base::key_vector_type key_vector_type;
 
+    using base::bind;
+    using base::operator();
+
     sc_map_list(const key_value_vector_type& key_vector, const sc_module_name name);
     template <typename config_T>
     sc_map_list(const key_value_vector_type& key_vector, const sc_module_name name, const config_T& configuration);
@@ -42,6 +45,12 @@ public:
     template <typename config_T>
     sc_map_list(const sc_module_name name, const std::map<key_value_T, config_T>& configuration);
     virtual ~sc_map_list() {};
+
+    template <typename signal_T>
+    void bind(sc_map_base<range_type, signal_T>& signal_map);
+    template <typename signal_T>
+    void operator()(sc_map_base<range_type, signal_T>& signal_map);
+    // TODO: add binding for iterators, which are not in the correct order
 };
 
 //******************************************************************************
@@ -129,6 +138,46 @@ sc_map_list<key_value_T, object_T>::sc_map_list(const sc_module_name name,
     }
 
     this->init(new_range, typename base::creator(), configuration_vec);
+
+    return;
+}
+
+//******************************************************************************
+template <typename key_value_T, typename object_T>
+template <typename signal_T>
+void sc_map_list<key_value_T, object_T>::bind(
+        sc_map_base<range_type, signal_T>& signal_map)
+{
+    // todo: check for equal size
+    // todo: check for same object
+    // todo: check for compatibility of port and signal (pre-processor)
+
+    for (typename sc_map_list<key_value_T, object_T>::iterator port_it = this->begin();
+         port_it != this->end();
+         ++port_it)
+    {
+        std::pair<bool, key_type> port_key = this->get_key(*port_it);
+        if (port_key.first)
+        {
+            typename sc_map_list<key_value_T, signal_T>::object_type& signal = signal_map.at(port_key.second);
+            port_it->bind(signal);
+        }
+        else
+        {
+            std::cerr << "sc_map_list.bind: Problem with iterator" << std::endl;
+        }
+    }
+
+    return;
+}
+
+//******************************************************************************
+template <typename key_value_T, typename object_T>
+template <typename signal_T>
+void sc_map_list<key_value_T, object_T>::operator()(
+        sc_map_base<range_type, signal_T>& signal_map)
+{
+    bind(signal_map);
 
     return;
 }
